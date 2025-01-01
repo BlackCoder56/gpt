89a2116b2ac87a4b53777075d9179cb46df7a6de
BlackCoder56 <blackcoda56@gmail.com>
2025-01-01 21:40:11 +0300

    count_vowels.py

--- count_vowels.py
+++ count_vowels.py
@@ -1,2 +1,13 @@
 def count_vowels(s: str) -> int:
-    pass
+    num = 0
+    cleaned = s.lower()
+    vowels = {'a', 'e', 'i', 'o', 'u'}  # Use a set for faster lookups
+
+    for i in cleaned:
+        if i in vowels:
+            num += 1
+    
+    return num
+
+
+print(count_vowels('Monopoly and lopisI'))  # Output: 7
