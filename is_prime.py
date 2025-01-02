85275a9007b678c82fb202b63228f0218ab226b1
BlackCoder56 <blackcoda56@gmail.com>
2025-01-02 17:36:44 +0300

    is_prime.py

---
+++ is_prime.py
@@ -0,0 +1,21 @@
+import math
+def is_prime(n: int) -> bool:
+	if  n<=1:
+		return False
+	if n==2 or n==3:
+		return True
+
+	if n%2==0 or n%3==0:
+		return False
+
+	i = 5
+	
+	while i <= math.sqrt(n):
+		if n%i==0 or n%(i+2)==0:
+			return False
+		i+=6
+	return True
+
+
+
+print(is_prime(12))
